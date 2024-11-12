c4ec271579ff3686b9fed3db6891b9c75d87f331
costargc <costa.rgc@gmail.com>
2024-11-11 19:17:13 -0500

    port change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var exphbs = require("express-handlebars");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.static(path.join(__dirname, "web")));
